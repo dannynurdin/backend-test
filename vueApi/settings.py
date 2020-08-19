b08de63464ae2da6602eff70448d4f4c7bb9ab7d
dannynurdin <dannyfachrul@gmail.com>
2020-08-19 17:02:05 +0700

    add staticfiles

--- vueApi/settings.py
+++ vueApi/settings.py
@@ -133,14 +133,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-PROJECT_ROOT =  os.path.dirname(os.path.abspath(__file__))
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static')
+    os.path.join(BASE_DIR, 'static')
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
